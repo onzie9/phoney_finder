24ad4adf8fe1c04ea2645d8644db84d42c9d45a2
Trevor McGuire <onzie9@gmail.com>
2023-08-02 07:10:55 +0300

    Restricted to NWL20 and recorded actual phonies in csv.

--- scraper.py
+++ scraper.py
@@ -12,10 +12,11 @@ with open('cleaned_words.pkl', 'rb') as f:
 
 
 def is_phony(ls: list, valid_words: list = words):
+    output = []
     for w in ls:
         if w not in valid_words and w[:8] != "EXCHANGE" and w != "LOST CHALLENGE":
-            return True
-    return False
+            output.append(w)
+    return output
 
 
 annotated_url = 'https://www.cross-tables.com/'
@@ -23,11 +24,13 @@ annotated_url = 'https://www.cross-tables.com/'
 data = []
 
 for x in range(1, 24):
+
     offset = str(x) + "01"
 
     r = requests.get("https://www.cross-tables.com/annolistself.php?offset=" + offset, headers=headers)
     soup = BeautifulSoup(r.text, "html.parser")
     for row in range(1, 101):
+        print([x, row, len(data)])
         broken = 0
         num_words = 0
         game = soup.find("tr", {"id": "row" + str(row)})
@@ -36,29 +39,32 @@ for x in range(1, 24):
         annotated_request = requests.get(annotated_url + url['href'], headers=headers)
         annotated_soup = BeautifulSoup(annotated_request.text, "html.parser")
 
-        player1 = str(annotated_soup).split("</div> -->")[1].split(" <a")[0]
-        player2 = str(annotated_soup).split("vs. ")[1].split(" <a")[0]
+        dictionary = str(annotated_soup).split('Dictionary: <b>')[1].split('</b')[0]
 
-        for i in range(20, 40):  # Crudely find the number of plays in a game.
-            num_words = i
-            if not annotated_soup.find("a", {"id": "moveselector" + str(i)}):
-                num_words = i-1
-                break
-        played_words = []
-        for i in range(num_words):
-            try:
-                word = str(annotated_soup).split('showmove(' + str(i+1) + '); scroll(0,0);">')[1].split("</a>")[0].upper()
-                played_words.append(word)
-            except IndexError:
-                broken = 1
-                break
-        if broken != 0:
-            broken = 0
-        else:
-            datum = [player1[4:], player2, url["href"]] + [played_words[:-1]] + [is_phony(played_words[:-1])]
+        if dictionary == "NWL20":
+            player1 = str(annotated_soup).split("</div> -->")[1].split(" <a")[0]
+            player2 = str(annotated_soup).split("vs. ")[1].split(" <a")[0]
 
-            data.append(datum)
+            for i in range(20, 40):  # Crudely find the number of plays in a game.
+                num_words = i
+                if not annotated_soup.find("a", {"id": "moveselector" + str(i)}):
+                    num_words = i-1
+                    break
+            played_words = []
+            for i in range(num_words):
+                try:
+                    word = str(annotated_soup).split('showmove(' + str(i+1) + '); scroll(0,0);">')[1].split("</a>")[0].upper()
+                    played_words.append(word)
+                except IndexError:
+                    broken = 1
+                    break
+            if broken != 0:
+                broken = 0
+            elif len(is_phony(played_words[:-1])) > 0:
+                datum = [player1[4:], player2, url["href"]] + [played_words[:-1]] + [is_phony(played_words[:-1])]
 
-df = pd.DataFrame(columns=["Player1", "Player2", "Game URL", "Played Words", "Phony Played"], data=data)
+                data.append(datum)
+
+df = pd.DataFrame(columns=["Player1", "Player2", "Game URL", "Played Words", "Phonies Played"], data=data)
 
 df.to_csv("phoney_finds.csv", index=False)
